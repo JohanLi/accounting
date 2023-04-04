f887437efafb231e76e3ad11e07557a5af413481
Johan Li <hi@johan.li>
2023-04-04 23:15:06 +0200

    upload endpoint should return a promise

--- src/pages/api/upload.ts
+++ src/pages/api/upload.ts
@@ -15,45 +15,55 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     return
   }
 
-  form.parse(req, async (err, fields, files) => {
-    let documents = files[UPLOAD_FORM_KEY]
+  return new Promise<void>((resolve) => {
+    form.parse(req, async (err, fields, files) => {
+      if (err) {
+        console.error(err)
+        res.status(500)
+        resolve()
+        return
+      }
 
-    if (!Array.isArray(documents)) {
-      documents = [documents]
-    }
+      let documents = files[UPLOAD_FORM_KEY]
 
-    for (const document of documents) {
-      const extension = document.originalFilename?.split('.').pop() || ''
-      const hash = document.hash || ''
-      const file = await readFile(document.filepath)
+      if (!Array.isArray(documents)) {
+        documents = [documents]
+      }
 
-      try {
-        const receipt = await parse(document.filepath)
+      for (const document of documents) {
+        const extension = document.originalFilename?.split('.').pop() || ''
+        const hash = document.hash || ''
+        const file = await readFile(document.filepath)
 
-        await prisma.verification.create({
-          data: {
-            date: receipt.date,
-            description: receipt.description,
-            transactions: {
-              create: receiptToTransaction(receipt),
-            },
-            documents: {
-              create: {
-                extension,
-                hash,
-                file,
+        try {
+          const receipt = await parse(document.filepath)
+
+          await prisma.verification.create({
+            data: {
+              date: receipt.date,
+              description: receipt.description,
+              transactions: {
+                create: receiptToTransaction(receipt),
+              },
+              documents: {
+                create: {
+                  extension,
+                  hash,
+                  file,
+                },
               },
             },
-          },
-        })
-      } catch (e) {
-        console.error(e)
-      } finally {
+          })
+        } catch (e) {
+          console.error(e)
+        }
+
         await unlink(document.filepath)
       }
-    }
 
-    res.status(200).json({})
+      res.status(200).json({})
+      resolve()
+    })
   })
 }
 
