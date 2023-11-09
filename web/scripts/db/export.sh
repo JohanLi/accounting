c410b2fb3680dbd24d8438de9b622e7d392bd1b0
Johan Li <hi@johan.li>
2023-11-09 21:26:26 +0100

    append a timestamp to database backup filenames

--- web/scripts/db/export.sh
+++ web/scripts/db/export.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 # https://stackoverflow.com/a/4774063
-SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+scriptPath="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+path="$scriptPath/backup.sql"
+pathWithTimestamp="$scriptPath/backup-$(date +%Y-%m-%dT%H:%M:%S).sql"
 
-docker exec accounting-postgres-1 pg_dump -U postgres --data-only -d postgres -n public > "${SCRIPTPATH}/backup.sql"
+docker exec accounting-postgres-1 pg_dump -U postgres --data-only -d postgres -n public > $path
+cp "$path" "$pathWithTimestamp"
