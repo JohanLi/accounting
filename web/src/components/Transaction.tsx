c353997cc88e220bec646b560e86d509c209c0f4
Johan Li <hi@johan.li>
2023-12-03 08:25:42 +0100

    remove ability to link from within a transaction

--- web/src/components/Transaction.tsx
+++ web/src/components/Transaction.tsx
@@ -1,34 +1,14 @@
-'use client'
-
-import { useState } from 'react'
 import { InferSelectModel } from 'drizzle-orm'
 import { Transactions } from '../schema'
 import { DateFormatted } from './DateFormatted'
 import { Amount } from './Amount'
 import { LinkIcon } from '@heroicons/react/20/solid'
-import { Button } from './Button'
-import { TransactionLinkForm } from './TransactionLinkForm'
 
 type Props = {
   transaction: InferSelectModel<typeof Transactions>
 }
 
 export function Transaction({ transaction }: Props) {
-  const [editLink, setEditLink] = useState(false)
-
-  if (editLink) {
-    return (
-      <tr>
-        <td colSpan={6}>
-          <TransactionLinkForm
-            transaction={transaction}
-            onClose={() => setEditLink(false)}
-          />
-        </td>
-      </tr>
-    )
-  }
-
   return (
     <tr>
       <td className="whitespace-nowrap py-4 pr-3 text-xs text-gray-500">
@@ -44,28 +24,7 @@ export function Transaction({ transaction }: Props) {
         <Amount amount={transaction.balance} />
       </td>
       <td className="relative whitespace-nowrap py-4 text-right text-xs">
-        {transaction.journalEntryId && (
-          <a
-            href="#"
-            className="inline-flex items-center text-gray-500 hover:text-gray-800"
-            onClick={(e) => {
-              e.preventDefault()
-
-              setEditLink(true)
-            }}
-          >
-            <LinkIcon className="h-4 w-4" />
-          </a>
-        )}
-      </td>
-      <td className="text-right">
-        {!transaction.journalEntryId && (
-          <Button
-            type="secondary"
-            onClick={() => setEditLink(true)}
-            text="Add link"
-          />
-        )}
+        {transaction.journalEntryId && <LinkIcon className="h-4 w-4" />}
       </td>
     </tr>
   )
