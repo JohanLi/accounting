bd4b68d7c171ad992a50cea0a7b8b250fb9ec5d4
Johan Li <hi@johan.li>
2025-10-13 19:39:09 +0200

    set a z-index for the download widget

--- extension/components/download.tsx
+++ extension/components/download.tsx
@@ -147,8 +147,9 @@ export default function Download({ getDownloads, requestInit }: Props) {
     </button>
   )
 
+  // SEB has a z-index 1000 footer that otherwise would cover this
   return (
-    <div className="fixed right-4 bottom-4 h-32 w-64 rounded-lg bg-white p-4 font-sans shadow-lg ring-1 ring-black/5">
+    <div className="fixed right-4 bottom-4 z-[9999] h-32 w-64 rounded-lg bg-white p-4 font-sans shadow-lg ring-1 ring-black/5">
       {state.error && (
         <>
           <div>{state.error}</div>
