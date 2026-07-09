0add32864d72fabd45f863077514304213b2bf9e
Johan Li <hi@johan.li>
2026-07-09 23:47:44 +0200

    run lint-staged from workspace root

--- web/.lintstagedrc.mjs
+++ web/.lintstagedrc.mjs
@@ -1,9 +1,13 @@
 // https://nextjs.org/docs/app/api-reference/config/eslint#running-lint-on-staged-files
 import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const buildEslintCommand = (filenames) =>
   `next lint --fix --file ${filenames
-    .map((f) => path.relative(process.cwd(), f))
+    .map((f) => path.relative(__dirname, f))
     .join(' --file ')}`
 
 const config = {
